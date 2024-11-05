fed457aa3de46126482c885c8bc17d39dda4a271
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-11-06 00:54:41 +0200

    dynamic styling with passing true false prop isSelected to jsx button comp

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,62 @@ import TabButton from './components/TabButton/TabButton.jsx'
 import { EXAMPLES } from './data-with-examples.js';
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  function clickHandler(selectedButton){
+  function clickHandler(selectedButton) {
     setSelectedTopic(selectedButton);
-    
-    } 
-    // shorter syntax with spread ... operator. Works only if prop names in component are the same as the given from the data array
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = <div id='tab-content'>
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+  }
+
+  // shorter syntax with spread ... operator. Works only if prop names in component are the same as the given from the data array
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept
-          title={CORE_CONCEPTS[0].title}
-          image={CORE_CONCEPTS[0].image}
-          description={CORE_CONCEPTS[0].description}
-          />
-          <CoreConcept {...CORE_CONCEPTS[1]}/> 
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>      
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => clickHandler('components')}>Component</TabButton>
-            <TabButton onSelect={() => clickHandler('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => clickHandler('props')}>Props</TabButton>
-            <TabButton onSelect={() => clickHandler('state')}>State</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'components'} 
+            onSelect={() => clickHandler('components')}>Component
+            </TabButton>
+            <TabButton
+             isSelected={selectedTopic === 'jsx'} 
+              onSelect={() => clickHandler('jsx')}>JSX
+            </TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'props'}  
+            onSelect={() => clickHandler('props')}>Props
+            </TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'state'} 
+            onSelect={() => clickHandler('state')}>State
+            </TabButton>
           </menu>
-          <div id='examples'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
