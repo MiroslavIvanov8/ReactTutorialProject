74c0cbfc7ecdbc06a8467c5c7c2eee45801fd675
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-11-05 15:17:44 +0200

    Dynamically load different examples with useState react hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data-with-examples.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx'
+import { EXAMPLES } from './data-with-examples.js';
 
 function App() {
-  function clickHandler(){
-    console.log('Hello World! - selected');
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
+
+  function clickHandler(selectedButton){
+    setSelectedTopic(selectedButton);
+    
     } 
     // shorter syntax with spread ... operator. Works only if prop names in component are the same as the given from the data array
   return (
@@ -28,11 +33,18 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={clickHandler}>Component</TabButton>
-            <TabButton onSelect={clickHandler}>JSX</TabButton>
-            <TabButton onSelect={clickHandler}>Props</TabButton>
-            <TabButton onSelect={clickHandler}>State</TabButton>
+            <TabButton onSelect={() => clickHandler('components')}>Component</TabButton>
+            <TabButton onSelect={() => clickHandler('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => clickHandler('props')}>Props</TabButton>
+            <TabButton onSelect={() => clickHandler('state')}>State</TabButton>
           </menu>
+          <div id='examples'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
